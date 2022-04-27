85f9e72f7445b6b513cae7482f5471e038365e4c
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-04-27 18:07:09 +0200

    View in View

---
+++ chapters/ch_03/webapp/view/JavaScriptView.view.js
@@ -0,0 +1,17 @@
+sap.ui.define([
+    "sap/m/Label"
+], function (
+    Label
+) {
+    "use strict";
+    sap.ui.jsview("sap.ui.demo.ch_03.view.JavaScriptView", {
+        getControllerName: function () {
+            return "sap.ui.demo.ch_03.controller.JavaScriptView";
+        },
+        createContent: function (oController) {
+            var oLabel = new Labeln();
+            oLabel.setText("Hallo JavaScript");
+            return oLabel;
+        }
+    });
+});
