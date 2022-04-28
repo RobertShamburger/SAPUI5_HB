2c171f0a45e4560f85927552caf9e7306ad3c9e7
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-04-28 17:37:10 +0200

    missing brackett

--- chapters/ch_03/webapp/controller/App.controller.js
+++ chapters/ch_03/webapp/controller/App.controller.js
@@ -1,17 +1,18 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/demo/ch_03/model/models"
-], function(
+], function (
 	Controller,
 	models
 ) {
 	"use strict";
 
-	return Controller.extend("sap.ui.demo.ch_03.controller.App", 
-    {
-      onInit : function() {
-		  var oData = models.createDataModel();
-		  this.getView().setModel(oData);
-	  }  
+	return Controller.extend("sap.ui.demo.ch_03.controller.App",
+		{
+			onInit: function () {
+				var oData = models.createDataModel();
+				this.getView().setModel(oData);
+			}
+		}  
 	});
 });
