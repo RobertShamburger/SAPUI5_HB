b8bb1c26339f3dd05525cfb2a0142f48354afae7
RobertShamburger <58690396+RobertShamburger@users.noreply.github.com>
2022-11-18 16:28:47 +0100

    update

--- kapitel/kapitel03/webapp/controller/App.controller.js
+++ kapitel/kapitel03/webapp/controller/App.controller.js
@@ -1,10 +1,14 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
-], function (Controller) {
+    "sap/ui/core/mvc/Controller",
+    "de/sapui5buch/demo/model/models"
+], function (Controller, models) {
     'use strict';
     return Controller.extend("de.sapui5buch.demo.controller.App", {
 
-        onInit: function () { },
+        onInit: function () { 
+            var oData = models.createDataModel( );
+            this.getView().setModel(oData);
+        },
         onExit: function () { },
         onBeforeRendering: function () { },
         onAfterRendering: function () { }
